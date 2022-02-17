b2ffefa115a3dbdb09a9f9389e89b6f142fb1d74
Jeff <jeff_patterson@outlook.com>
2022-02-16 18:41:40 -0600

    Lesson 6 completed

--- Lesson5/main.c
+++ Lesson5/main.c
@@ -1,5 +1,7 @@
+#include <stdint.h>
 #include "tm4c123gh6pm.h"
 
+/*
 #define GPIO_BASE 0x40025000U
 
 /// 0x20U; //Enable Memory block P4 scanning with bit 5 high (Clock)
@@ -11,41 +13,53 @@
 /// 0x0EU; //Enable GPIO Digital Function Port F bits 1, 2, and 3
 #define GPIOF_DEN  (*((unsigned int*)(GPIO_BASE + 0x51CU)))
 
-
 #define GPIOF_DATA (*((unsigned int*)(GPIO_BASE + 0x3FCU)))
+*/
 
 void delayLoop(int loopCount){
-  int counter = 0;
+  int volatile counter = 0;
   while (counter < loopCount){
     ++counter;
   }
 }
 
 int main() {
+  SYSCTL_RCGCGPIO_R = 0x20U; /* Enable clock for GPIOF */
+  GPIO_PORTF_DIR_R = 0x0EU; /* Set port F pins 1, 2, and 3 as output */
+  GPIO_PORTF_DEN_R = 0x0EU; /*  */
+
+  /*
   RCGCGPIO = 0x20U;  //Enable Memory block P4 scanning with bit 5 high (Clock)
   GPIOF_DIR = 0x0EU; //Enable GPIO Pin direction for Port F bits 1, 2, and 3 as OUTPUT
   GPIOF_DEN= 0x0EU;  //Enable GPIO Digital Function Port F bits 1, 2, and 3
+*/
 
-  int counter = 0;
+
   int loopCount = 1000000;
 
   while(1) {
-    GPIOF_DATA = 0x02U; //Turn ON bit 1 (Red LED)
+    GPIO_PORTF_DATA_R = 0x02U;
+    //GPIOF_DATA = 0x02U; //Turn ON bit 1 (Red LED)
     delayLoop(loopCount);
 
-    GPIOF_DATA = 0x00U; //Turn OFF bit 1 (Red LED)
+    GPIO_PORTF_DATA_R = 0x00U;
+    //GPIOF_DATA = 0x00U; //Turn OFF bit 1 (Red LED)
     delayLoop(loopCount);
 
-    GPIOF_DATA = 0x04U; //Turn ON bit 2 (Blue LED)
+    GPIO_PORTF_DATA_R = 0x04U;
+    //GPIOF_DATA = 0x04U; //Turn ON bit 2 (Blue LED)
     delayLoop(loopCount);
 
-    GPIOF_DATA = 0x00U; //Turn OFF bit 2 (Blue LED)
+    GPIO_PORTF_DATA_R = 0x00U;
+    //GPIOF_DATA = 0x00U; //Turn OFF bit 2 (Blue LED)
     delayLoop(loopCount);
 
-    GPIOF_DATA = 0x08U; //Turn ON bit 3 (Breen LED)
+    GPIO_PORTF_DATA_R = 0x08U;
+    //GPIOF_DATA = 0x08U; //Turn ON bit 3 (Breen LED)
     delayLoop(loopCount);
 
-    GPIOF_DATA = 0x00U; //Turn OFF bit 3 (Breen LED)
+    GPIO_PORTF_DATA_R = 0x00U;
+    //GPIOF_DATA = 0x00U; //Turn OFF bit 3 (Breen LED)
     delayLoop(loopCount);
   }
 
--- Lesson6/main.c
+++ Lesson6/main.c
@@ -2,19 +2,27 @@
 #include "tm4c123gh6pm.h"
 
 /*
-#define GPIO_BASE 0x40025000U
+GPIO_PORTF_DATA_R   XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX
+LED_RED             0000 0000 0000 0000 0000 0000 0000 0010
+------------------------------------------------------------
+GPIO ... | LED_RED  XXXX XXXX XXXX XXXX XXXX XXXX XXXX XX1X // OR the 2 words together
 
-/// 0x20U; //Enable Memory block P4 scanning with bit 5 high (Clock)
-#define RCGCGPIO  (*((unsigned int*)0x400FE608U))
+GPIO_PORTF_DATA_R   XXXX XXXX XXXX XXXX XXXX XXXX XXXX XXXX
+~LED_RED            1111 1111 1111 1111 1111 1111 1111 1101
+------------------------------------------------------------
+GPIO ... & ~LED_RED XXXX XXXX XXXX XXXX XXXX XXXX XXXX XX0X // AND the 2 words together
 
-/// 0x0EU; //Enable GPIO Pin direction for Port F bits 1, 2, and 3
-#define GPIOF_DIR  (*((unsigned int*)(GPIO_BASE + 0x400U)))
+ LED_BLU             0000 0000 0000 0000 0000 0000 0000 0100
+~LED_BLU             1111 1111 1111 1111 1111 1111 1111 1011
 
-/// 0x0EU; //Enable GPIO Digital Function Port F bits 1, 2, and 3
-#define GPIOF_DEN  (*((unsigned int*)(GPIO_BASE + 0x51CU)))
-
-#define GPIOF_DATA (*((unsigned int*)(GPIO_BASE + 0x3FCU)))
+ LED_GRN             0000 0000 0000 0000 0000 0000 0000 1000
+~LED_GRN             1111 1111 1111 1111 1111 1111 1111 0111
 */
+
+#define LED_OFF 0
+#define LED_RED (1U << 1)
+#define LED_BLU (1U << 2)
+#define LED_GRN (1U << 3)
 
 void delayLoop(int loopCount){
   int volatile counter = 0;
@@ -24,42 +32,50 @@ void delayLoop(int loopCount){
 }
 
 int main() {
-  SYSCTL_RCGCGPIO_R = 0x20U; /* Enable clock for GPIOF */
-  GPIO_PORTF_DIR_R = 0x0EU; /* Set port F pins 1, 2, and 3 as output */
-  GPIO_PORTF_DEN_R = 0x0EU; /*  */
-
   /*
-  RCGCGPIO = 0x20U;  //Enable Memory block P4 scanning with bit 5 high (Clock)
-  GPIOF_DIR = 0x0EU; //Enable GPIO Pin direction for Port F bits 1, 2, and 3 as OUTPUT
-  GPIOF_DEN= 0x0EU;  //Enable GPIO Digital Function Port F bits 1, 2, and 3
-*/
+  unsigned int a = 0x5A5A5A5A;
+  unsigned int b = 0xDEADBEEF;
+  unsigned int c;
 
+  c = a | b;  // OR
+  c = a & b;  // AND
+  c = a ^ b;  // XOR
+  c = ~b;     // NOT
+  c = b >> 1; // Right Shift
+  c = b << 3; // Left Shift
+
+  int x = 1024;
+  int y = -1024;
+  int z;
+
+  z = x >> 3;
+  z = y << 3;
+  */
+
+  SYSCTL_RCGCGPIO_R |= (1U << 5); /* Enable clock for GPIOF = 0x20 */
+  GPIO_PORTF_DIR_R  |= (LED_RED | LED_BLU | LED_GRN); /* Set port F pins 1, 2, and 3 as output */
+  GPIO_PORTF_DEN_R  |= (LED_RED | LED_BLU | LED_GRN); /*  */
 
   int loopCount = 1000000;
 
   while(1) {
-    GPIO_PORTF_DATA_R = 0x02U;
-    //GPIOF_DATA = 0x02U; //Turn ON bit 1 (Red LED)
+    //GPIO_PORTF_DATA_R = GPIO_PORTF_DATA_R | LED_RED;
+    GPIO_PORTF_DATA_R = LED_RED; //To work this register must have R/W permission
     delayLoop(loopCount);
 
-    GPIO_PORTF_DATA_R = 0x00U;
-    //GPIOF_DATA = 0x00U; //Turn OFF bit 1 (Red LED)
+    GPIO_PORTF_DATA_R &= ~LED_RED; //Clear the bit (Turn OFF the LED)
     delayLoop(loopCount);
 
-    GPIO_PORTF_DATA_R = 0x04U;
-    //GPIOF_DATA = 0x04U; //Turn ON bit 2 (Blue LED)
+    GPIO_PORTF_DATA_R = LED_BLU;
     delayLoop(loopCount);
 
-    GPIO_PORTF_DATA_R = 0x00U;
-    //GPIOF_DATA = 0x00U; //Turn OFF bit 2 (Blue LED)
+    GPIO_PORTF_DATA_R &= ~LED_BLU;
     delayLoop(loopCount);
 
-    GPIO_PORTF_DATA_R = 0x08U;
-    //GPIOF_DATA = 0x08U; //Turn ON bit 3 (Breen LED)
+    GPIO_PORTF_DATA_R = LED_GRN;
     delayLoop(loopCount);
 
-    GPIO_PORTF_DATA_R = 0x00U;
-    //GPIOF_DATA = 0x00U; //Turn OFF bit 3 (Breen LED)
+    GPIO_PORTF_DATA_R &= ~LED_GRN;
     delayLoop(loopCount);
   }
 
